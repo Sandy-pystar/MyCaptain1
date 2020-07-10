1b82e93bdfa1d94d9361ca712e9f520fd25740ba
Sandy-pystar <68112848+Sandy-pystar@users.noreply.github.com>
2020-07-10 21:01:48 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,3 @@
+r=float(input("enter a number: "))
+area=3.14*(r*r)
+print("the area is:",area)
---
+++ ext.py
@@ -0,0 +1,3 @@
+fn=input("Enter the filename : ")
+ext= fn.split(".")
+print("The extension of the filename is: "+ext[-1])
